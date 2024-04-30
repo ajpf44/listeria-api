3f65149e920486667bf4c8705133d0bb69a988e1
Alexandre Ferreira <ajpf44@gmail.com>
2024-04-29 21:14:58 -0300

    criando repositorio

---
+++ src/main/java/com/ajpf/listeria/ListeriaApplication.java
@@ -0,0 +1,13 @@
+package com.ajpf.listeria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListeriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListeriaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ajpf/listeria/ListeriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ajpf.listeria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListeriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
